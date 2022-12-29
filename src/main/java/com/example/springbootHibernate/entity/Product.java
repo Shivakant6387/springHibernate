24c96fcb738d1c1088fcf5225c28dfaf87d72d43
Shivakant6387 <shivakant5875@gmail.com>
2022-12-29 18:08:05 +0530

    added spring boot hibernate product

---
+++ src/main/java/com/example/springbootHibernate/entity/Product.java
@@ -0,0 +1,29 @@
+package com.example.springbootHibernate.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+
+@Entity
+@Table(name = "PRODUCT_TABLE")
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+
+//    public Product() {
+//    }
+}
---
+++ src/test/java/com/example/springbootHibernate/SpringbootHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
